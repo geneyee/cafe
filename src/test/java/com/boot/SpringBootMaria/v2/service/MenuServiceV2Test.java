6357d3aff733e890be4bff894df07fcd17a65e76
geneyee <geneyeey@gmail.com>
2023-10-05 15:00:17 +0900

    menuService test

---
+++ src/test/java/com/boot/SpringBootMaria/v2/service/MenuServiceV2Test.java
@@ -0,0 +1,26 @@
+package com.boot.SpringBootMaria.v2.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MenuServiceV2Test {
+
+    @Autowired
+    MenuServiceV2 menuServiceV2;
+
+    @Test
+    void doUpdateInsert() {
+        List<String> chkList = new ArrayList<>();
+        chkList.add("1");
+        chkList.add("2");
+        String price = "1500";
+        menuServiceV2.doUpdateInsert(chkList, price);
+    }
+}
